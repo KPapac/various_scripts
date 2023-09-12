39e773bbdc7ea8d1c158c2bc8136fc90f40ef28e
Konstantinos Papachristos <konstantinos.papachristos@icm.uu.se>
2023-09-12 14:05:36 +0200

    Added a function in SummariseGBK.py that calculates the total number of bases for CDS features.

--- SummariseGBK.py
+++ SummariseGBK.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import re
 from Bio import GenBank
 
 def summarise_GBK(gbkFile):
@@ -12,7 +13,24 @@ def summarise_GBK(gbkFile):
                 if feature.key not in feature_summary.keys():
                     feature_summary[feature.key]=1 
                 else: feature_summary[feature.key]+=1
-    return f"{gbkFile}: {feature_summary}"
+    return feature_summary
+
+def getLenCoding(gbkFile):
+    perc_coding=0
+    with open(gbkFile, 'r') as handle:
+        for record in GenBank.parse(handle):
+            for feature in record.features:
+                if feature.key == 'CDS': 
+                    start, end, *remaining_coords = re.findall(r'\d+', feature.location)
+                    while remaining_coords:
+                        start, end = int(start), int(end)
+                        CDS_length=end-start+1
+                        perc_coding+=CDS_length
+                        start, end, *remaining_coords = remaining_coords
+                    start, end = int(start), int(end)
+                    CDS_length=end-start+1
+                    perc_coding+=CDS_length
+    return perc_coding
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse GBK files and optionally write the output to a file.")
@@ -23,7 +41,7 @@ if __name__ == "__main__":
 
     summary=''
     for gbk_file in args.input_files:
-        summary+=summarise_GBK(gbk_file) + '\n'
+        summary+=f"{gbk_file}: {summarise_GBK(gbk_file)} {getLenCoding(gbk_file)}" + '\n'
     if args.output_file == None: print(summary)
     else: 
         with open(args.output_file, 'w') as f:
